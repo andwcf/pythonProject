6408d34eb2096d16cba46326c58d6a4b0998b9f7
andwcf <90454172+andwcf@users.noreply.github.com>
2021-09-10 12:43:08 +0100

    Second commit

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 """This is my first github project"""
 
 print("Hello World")
+
+teams = ['united', 'city', 'forrest']
+new_teams = []
+
+def sort_teams(teams, new_teams):
+    while teams:
+        for team in teams:
+            moving_team=teams.pop()
+            new_teams.append(moving_team)
+
+
+sort_teams(teams, new_teams)
+
+print(f"First list of teams, {teams}")
+print(f"Second list of teams, {new_teams}")
